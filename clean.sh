3ab9e9dc3940f89f3f4788275cbbad991b633671
quocbao <quocbao747@gmail.com>
2017-04-21 23:54:22 +0700

    Stable version 1.0

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm *.o 2> /dev/null
-rm *.save 2> /dev/null
-rm *.layout 2> /dev/null
-rm *.xml 2> /dev/null
-rm webserver 2> /dev/null
--- sh/clean.sh
+++ sh/clean.sh
@@ -4,4 +4,4 @@ rm *.o 2> /dev/null
 rm *.save 2> /dev/null
 rm *.layout 2> /dev/null
 rm *.xml 2> /dev/null
-rm webserver 2> /dev/null
+rm html/webserver 2> /dev/null
